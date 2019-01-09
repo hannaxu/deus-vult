9ec8cb8560b8aa881f1e7f7a128ae8faf9bc1869
Richard Zhan <richymanzhan@gmail.com>
2019-01-09 15:50:14 -0500

    pathfinding info

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # deus-vult
+# this.findMove(start, end):
+# This method intakes two arrays of length two, which represent the x/y coordinates of the start/end positions
+# It returns an array of length two which is the dx/dy that the robot should move.
+# This method accounts for unit collision and won't return impossible moves, but it may use more fuel than we have.
+# 
--- crusader.js
+++ crusader.js
@@ -1,7 +1,7 @@
 import vars from './variables';
 
 export default function crusaderTurn() {
-    this.log("I am a Crusader at "+vars.xpos+" "+vars.ypos);
-    var choice = vars.moveable[Math.floor(Math.random()*vars.moveable.length)];
-    return this.move(choice);
+  this.log("I am a Crusader at "+vars.xpos+" "+vars.ypos);
+  var choice = vars.moveable[Math.floor(Math.random()*vars.moveable.length)];
+  return this.move(choice);
 }
