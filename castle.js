3b56ee8c3f957acbee629c27821b11b2e82c3510
etherlyt <hcx2001@gmail.com>
2019-01-10 22:22:51 -0500

    add headcount

--- castle.js
+++ castle.js
@@ -2,11 +2,10 @@ import vars from './variables';
 import * as utils from './utils';
 
 var team;
-var castles;
-var numC;
+var totC;
 
 export default function castleTurn() {
-  this.log("I am a Castle at "+this.me.x+" "+this.me.y);
+  //this.log("I am a Castle at "+this.me.x+" "+this.me.y);
   //this.log("Resources: "+vars.firstTurn);
   if (vars.firstTurn) {
     var symmetry = utils.checkMapSymmetry(vars.passableMap, vars.karbMap, vars.fuelMap);
@@ -16,15 +15,32 @@ export default function castleTurn() {
     //determine if enemy castles are visible and number
     //  try to determine if map is truly horizontal or vertical if symmetry returned both
     team = this.me.team;
-    castles = this.getVisibleRobots();
-    numC = castles.length;
-    for( var x = 0; x < numC; x++ ) {
-      if( castles[x].team != team ) {
-        numC = numC-1;
+    totC = vars.visibleRobots.length;
+    for( var x = 0; x < totC; x++ ) {
+      if( vars.visibleRobots[x].team != team ) {
+        totC = totC-1;
       }
     }
     vars.firstTurn = false;
     //this.log("Test: " +vars.firstTurn);
+  }
+
+  //headcount 0: castle, 1: church, 2: pilgrim, 3: crusader, 4: prophet, 5: preacher
+  var headcount = [0,0,0,0,0,0];
+  for( var i = 0; i < vars.visibleRobots.length; i++ ) {
+    if( vars.visibleRobots[i].team == team ) {
+      var u = vars.visibleRobots[i].unit;
+        if( u == vars.SPECS.CASTLE )
+          headcount[0] += 1;
+        if( u == vars.SPECS.CHURCH )
+          headcount[1] += 1;
+        if( u == vars.SPECS.PILGRIM )
+          headcount[2] += 1;
+        if( u == vars.SPECS.CRUSADER )
+          headcount[3] += 1;
+        if( u == vars.SPECS.PROPHET )
+          headcount[4] += 1;
+    }
   }
 
   if (this.karbonite >= 10 && this.fuel >= 50) {
