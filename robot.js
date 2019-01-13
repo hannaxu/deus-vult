18fc1bcb17f50de614d45effb1e7c28b6fcb22bf
Richard Zhan <richymanzhan@gmail.com>
2019-01-13 17:12:23 -0500

    deleted deprecated methods

--- robot.js
+++ robot.js
@@ -65,7 +65,7 @@ class MyRobot extends BCAbstractRobot {
           for (var i = 0; i < vars.buildable.length; i++) {
             var x = this.me.x+vars.buildable[i][0];
             var y = this.me.y+vars.buildable[i][1];
-            if (!this.checkBounds(x, y)) continue;
+            if (!utils.checkBounds(x, y)) continue;
             var id = this.getRobot(vars.visibleRobotMap[y][x]);
             if (id==null||id.signal==-1||(id.unit!=vars.SPECS.CASTLE&&id.unit!=vars.SPECS.CHURCH)) continue;
             var dir = vars.buildable[cypherMessage(id.signal, this.me.team)%vars.buildable.length];
@@ -142,59 +142,6 @@ class MyRobot extends BCAbstractRobot {
       this.log("Error "+err);
     }
   }
-
-  checkBounds(x, y) {
-    return 0 <= x && x < vars.xmax && 0 <= y && y < vars.ymax;
-  }
-
-  findMove(start, end) {
-    if (vars.fuzzyCost[end[0]][end[1]].length==0) {
-      for (var x = 0; x < vars.xmax; x++) {
-        vars.fuzzyCost[end[0]][end[1]].push([]);
-        for (var y = 0; y < vars.ymax; y++) {
-          vars.fuzzyCost[end[0]][end[1]][x].push(null);
-        }
-      }
-      this.bfs(end);
-    }
-    var bestMove = [vars.POS_INF, vars.POS_INF, null];
-    for (var i = 0; i < vars.moveable.length; i++) {
-      var x = this.me.x+vars.moveable[i][0];
-      var y = this.me.y+vars.moveable[i][1];
-      if (this.checkBounds(x, y)&&vars.passableMap[y][x]&&vars.visibleRobotMap[y][x]==0) {
-        var move = vars.fuzzyCost[end[0]][end[1]][x][y];
-        if (move[0]<bestMove[0]) {
-          bestMove = [move[0], move[1], vars.moveable[i]];
-        }
-        else if(move[0]==bestMove[0]&&move[1]<bestMove[0]) {
-          bestMove = [move[0], move[1], vars.moveable[i]];
-        }
-      }
-    }
-    return bestMove[2];
-  }
-
-  bfs (end) {
-    var index = 0;
-    var queue = [];
-    queue.push(end);
-    vars.fuzzyCost[end[0]][end[1]][end[0]][end[1]] = [0, 0];
-    while (index<queue.length) {
-      //this.log("q "+queue[index]);
-      var curCost = vars.fuzzyCost[end[0]][end[1]][queue[index][0]][queue[index][1]];
-      for (var i = 0; i < vars.moveable.length; i++) {
-        var x = queue[index][0]+vars.moveable[i][0];
-        var y = queue[index][1]+vars.moveable[i][1];
-        if (this.checkBounds(x, y)&&vars.passableMap[y][x]&&vars.fuzzyCost[end[0]][end[1]][x][y]==null) {
-          queue.push([x, y]);
-          vars.fuzzyCost[end[0]][end[1]][x][y] = [curCost[0]+1, curCost[1]+vars.moveCost*(vars.moveable[i][0]**2+vars.moveable[i][1]**2)];
-        }
-      }
-      index++;
-    }
-  }
-
-
 }
 
 MyRobot.prototype.castleTurn = castleTurn;
