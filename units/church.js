063acc1cb79d08bc2597c6d7b12af0774e5b7aa2
etherlyt <hcx2001@gmail.com>
2019-01-20 00:48:24 -0500

    standardize build

--- units/church.js
+++ units/church.js
@@ -3,20 +3,17 @@ import * as utils from '../utils';
 import { sendMessage } from '../communication';
 import * as buildUtils from '../buildUtils';
 
-var deposits = 0;
 var buildCount = [0,0,0,0,0,0];
 var team;
 var deposits = [0,[],[]]; //total, karb locs, fuel locs
-var buildOptPil = []; //pilgrims,
+var attackPos = null;
 
 export default function castleTurn() {
   //this.log("I am a Church at "+this.me.x+" "+this.me.y);
   //this.log("Resources: "+this.karbonite+" "+this.fuel);
   if (vars.firstTurn) {
     team = this.me.team;
-    var locs = buildUtils.optBuild(this.me.x, this.me.y);
-    deposits = locs[0];
-    buildOptPil = locs[1];
+    deposits = buildUtils.resources.call(this);
 
     vars.firstTurn = false;
   }
@@ -28,17 +25,21 @@ export default function castleTurn() {
         closePilgrim += 1;
   }
 
+  var visibleEnemies = buildUtils.findVisibleEnemies.call(this);
+  for (var i = 0; i < visibleEnemies.length; i++) {
+    if (visibleEnemies[i][2] != vars.SPECS.PILGRIM)
+      attackPos = [this.me.y+visibleEnemies[i][1], this.me.x+visibleEnemies[i][0]];
+  }
+
   if (this.karbonite >= vars.SPECS.UNITS[vars.SPECS.PILGRIM].CONSTRUCTION_KARBONITE && this.fuel >= vars.SPECS.UNITS[vars.SPECS.PILGRIM].CONSTRUCTION_FUEL) {
     if (closePilgrim < deposits[0] && headcount[4] >= vars.CHURCH_MIN_DEF) {
-      for (var i = 0; i < buildOptPil.length; i++) {
-        var x = this.me.x+buildOptPil[i][1];
-        var y = this.me.y+buildOptPil[i][0];;
-        if (utils.checkBounds(y, x)&&vars.passableMap[y][x]&&vars.visibleRobotMap[y][x]==0) {
-          sendMessage.call(this, i, buildOptPil[i][1]**2+buildOptPil[i][0]**2);
-          //this.log("Building pilgrim at "+x+" "+y);
-          buildCount[2]++;
-          return this.buildUnit(vars.SPECS.PILGRIM, buildOptPil[i][1], buildOptPil[i][0]);
-        }
+      var buildLoc = buildUtils.buildOpt.call(this, attackPos, deposits, vars.SPECS.PILGRIM, this.me.x, this.me.y);
+      //sendMessage.call(this, castleOrder, buildOptPil[i][1]**2+buildOptPil[i][0]**2);
+      //this.log("Building pilgrim at "+x+" "+y);
+      if( buildLoc != null ) {
+        buildCount[2]++;
+        vars.buildRobot = 2;
+        return this.buildUnit(vars.SPECS.PILGRIM, buildLoc[1], buildLoc[0]);
       }
     }
   }
@@ -46,14 +47,11 @@ export default function castleTurn() {
   // prophet build
   if (this.karbonite >= vars.SPECS.UNITS[vars.SPECS.PROPHET].CONSTRUCTION_KARBONITE && this.fuel >= vars.SPECS.UNITS[vars.SPECS.PROPHET].CONSTRUCTION_FUEL)  {
     if (headcount[4] < vars.CHURCH_MAX_DEF) {
-      for (var i = 0; i < vars.buildable.length; i++) {
-        var x = this.me.x+vars.buildable[i][0];
-        var y = this.me.y+vars.buildable[i][1];
-        if (utils.checkBounds(y, x)&&vars.passableMap[y][x]&&vars.visibleRobotMap[y][x]==0) {
-          //sendMessage.call(this, castleOrder, vars.buildable[i][0]**2+vars.buildable[i][1]**2);
-          //this.log("Building pilgrim at "+x+" "+y);
-          return this.buildUnit(vars.SPECS.PROPHET, vars.buildable[i][0], vars.buildable[i][1]);
-        }
+      var buildLoc = buildUtils.buildOpt.call(this, attackPos, deposits, vars.SPECS.PROPHET, this.me.x, this.me.y);
+      if( buildLoc != null ) {
+        buildCount[2]++;
+        vars.buildRobot = 4;
+        return this.buildUnit(vars.SPECS.PILGRIM, buildLoc[1], buildLoc[0]);
       }
     }
   }
