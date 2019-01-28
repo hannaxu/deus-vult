3d72bf4a034066a7f9c2eb1ad42cc6b6a57d20b2
Richard Zhan <richymanzhan@gmail.com>
2019-01-27 19:26:55 -0500

    decerase nav time

--- variables.js
+++ variables.js
@@ -9,7 +9,7 @@ export default {
 
   MIN_LAT_DIST: 2, // must be strictly greater than this
   DEFENSE_DEPTH: 10, // maximum search depth of navigate() when forming the defensive lattice
-  NAVIGATION_TIME_LIMIT: 100, // maximum amount of time used in navigate()
+  NAVIGATION_TIME_LIMIT: 50, // maximum amount of time used in navigate()
 
   ENEMY_PRIORITY: [5, 4, 3, 0, 1, 2],
 
